34a825ec6efd21d52d9b189c19c7b8ce6754bb14
Sanghoon Jeon <kppw99@gmail.com>
2021-03-30 11:15:43 +0000

    Add: initial build files for libtiff
    Signed-off-by: Sanghoon Jeon <kppw99@gmail.com>

---
+++ projects/libtiff/afl-fuzz.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+home=/targets/libtiff
+seed_home=$home/seed
+bin=$home/bin/bmp2tiff
+opt='-m 100 -t 10000+'
+
+origin_dir=$seed_home/org
+copt_dir=$seed_home/copt
+topt_dir=$seed_home/topt
+
+if [ -z $1 ]; then
+	input=$origin_dir
+elif [ $1 == 'cmin' ]; then
+	input=$copt_dir
+elif [ $1 == 'tmin' ]; then
+	input=$topt_dir
+else
+	echo ''
+	echo './afl-fuzz.sh {input_seed}'
+	echo ''
+	echo 'Required input seed:'
+	echo '  - cmin		: cmin directory'
+	echo '  - tmin		: tmin directory'
+	echo '  - default(null)	: origin directory'
+	echo ''
+	exit 100
+fi
+
+output=$home/output
+rm -rf $output
+afl-fuzz $opt -i $input -o $output -- $bin @@ /dev/null
