746b498781488058ea8d968d093a4e6052ffeabf
ehddud758 <aaa7663@naver.com>
2021-05-23 01:41:01 -0700

    libpng: modify build script_4

--- projects/libpng/opt-seed.sh
+++ projects/libpng/opt-seed.sh
@@ -12,7 +12,7 @@ topt_dir=$seed_home/topt
 if [ -z ${1} ] || [ ${1} == 'cmin' ]; then
 
 	rm -rf $copt_dir
-	LD_PRELOAD=$LIBRARY_PATH afl-cmin $opt -i $origin_dir -o $copt_dir -- $bin @@ /dev/null
+	afl-cmin $opt -i $origin_dir -o $copt_dir -- $bin @@ /dev/null
 
 	if [ -z $1 ]; then
 		for input in $copt_dir/*
@@ -21,7 +21,7 @@ if [ -z ${1} ] || [ ${1} == 'cmin' ]; then
 			output=$topt_dir/$filename
 
 			rm -rf $output
-			LD_PRELOAD=$LIBRARY_PATH afl-tmin $opt -i $input -o $output -- $bin @@ /dev/null
+			afl-tmin $opt -i $input -o $output -- $bin @@ /dev/null
 		done
 	fi
 elif [ $1 == 'tmin' ]; then
@@ -31,7 +31,7 @@ elif [ $1 == 'tmin' ]; then
 		output=$topt_dir/$filename
 
 		rm -rf $output
-		LD_PRELOAD=$LIBRARY_PATH afl-tmin $opt -i $input -o $output -- $bin @@ /dev/null
+		afl-tmin $opt -i $input -o $output -- $bin @@ /dev/null
 	done
 else
 	echo ''
