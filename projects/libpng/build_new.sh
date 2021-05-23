569c913dfa874b6e6e2c700776a5c3402b4e675a
ehddud758 <aaa7663@naver.com>
2021-05-23 01:18:15 -0700

    libpng: modify build script_3

--- projects/libpng/build_new.sh
+++ projects/libpng/build_new.sh
@@ -6,8 +6,7 @@ then
     CXXFLAGS="-fprofile-arcs -ftest-coverage"
     LDFLAGS="-fprofile-arcs -ftest-coverage -static"
     LDFLAGS_TEST="-fprofile-arcs -ftest-coverage"
-else
-    LDFLAGS="-static"
+
 fi
 
 CC=/tool/afl4ddrfuzz/afl-clang
@@ -17,15 +16,15 @@ OUT=.
 SRC=.
 
 make clean
-#CC=$CC CXX=$CXX CFALGS=$CFLAGS CXXFALGS=$CXXCFLAGS LDFLAGS=$LDFLAGS ./configure
-CC=$CC CXX=$CXX ./configure
+CC=$CC CXX=$CXX CFALGS=$CFLAGS CXXFALGS=$CXXCFLAGS ./configure
+#CC=$CC CXX=$CXX ./configure
 
 if [[ "$#" -ge "1" && ( $1 == "cov" ) ]];
 then
 	make -j
-	make check
+	#make check
 else
 	AFL_USE_ASAN=1 make -j
-	ASAN_OPTIONS=detect_leaks=0 AFL_USE_ASAN=1 make check
+	#ASAN_OPTIONS=detect_leaks=0 AFL_USE_ASAN=1 make check
 fi
 
