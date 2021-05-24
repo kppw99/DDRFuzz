6acf9a82ace50b596524043662bcc484366a900b
ehddud758 <ehddud758@gmail.com>
2021-05-24 02:25:13 -0700

    libpng: modify build sciprt_6

--- projects/libpng/build_new.sh
+++ projects/libpng/build_new.sh
@@ -2,21 +2,19 @@
 
 if [[ "$#" -ge "1" && $1 == "cov" ]];
 then
-    CFLAGS="-fprofile-arcs -ftest-coverage"
-    CXXFLAGS="-fprofile-arcs -ftest-coverage"
-    LDFLAGS="-fprofile-arcs -ftest-coverage -static"
-    LDFLAGS_TEST="-fprofile-arcs -ftest-coverage"
+	CC="/tool/afl4ddrfuzz/afl-clang -fprofile-arcs -ftest-coverage"
+	CXX="/tool/afl4ddrfuzz/afl-clang++ -fprofile-arcs -ftest-coverage"
 
+else
+	CC="/tool/afl4ddrfuzz/afl-clang"
+	CXX="/tool/afl4ddrfuzz/afl-clang++"
 fi
 
-CC=/tool/afl4ddrfuzz/afl-clang
-CXX=/tool/afl4ddrfuzz/afl-clang++
 nproc=1
 OUT=.
 SRC=.
 
 make clean
-CC=$CC CXX=$CXX CFALGS=$CFLAGS CXXFALGS=$CXXCFLAGS ./configure
-#CC=$CC CXX=$CXX ./configure
+CC=$CC CXX=$CXX ./configure
 
 make -j
