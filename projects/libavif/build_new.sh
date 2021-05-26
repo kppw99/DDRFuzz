160f519a8f28d443dad649563fa53de83cad4f7b
ehddud758 <ehddud758@gmail.com>
2021-05-26 00:42:09 -0700

    libavif: make build script

---
+++ projects/libavif/build_new.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ "$#" -ge "1" && $1 == "cov" ]];
+then
+	CC="/tool/afl4ddrfuzz/afl-gcc -fprofile-arcs -ftest-coverage"
+	CXX="/tool/afl4ddrfuzz/afl-g++ -fprofile-arcs -ftest-coverage"
+
+else
+	CC="/tool/afl4ddrfuzz/afl-gcc"
+	CXX="/tool/afl4ddrfuzz/afl-g++"
+fi
+
+nproc=1
+OUT=.
+SRC=.
+
+if [ -d build ];
+then
+	rm -rf ./build
+	mkdir build
+else
+	mkdir build
+fi
+
+
+cd build
+CC=$CC CXX=$CXX cmake -G Ninja -DAVIF_BUILD_EXAMPLES=on ..
+ninja
+
+
