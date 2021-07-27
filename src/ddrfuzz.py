2c8e3775b74f65d4bc93fd95f06d244fd1c591c0
Sanghoon Jeon <kppw99@gmail.com>
2021-07-27 22:11:18 +0900

    Modify: src >> ddrfuzz.py
    - fixed attention train and test code in ddrfuzz.py
    - fixed transformer train and test code in ddrfuzz.py

--- src/ddrfuzz.py
+++ src/ddrfuzz.py
@@ -15,7 +15,7 @@ if __name__=='__main__':
                         help='[seq2seq | attention | transformer]',
                         default='seq2seq')
     parser.add_argument('--mode', dest='mode', type=str,
-                        help='[train | test]', default='test')
+                        help='[train | test]', default='train')
     parser.add_argument('--path', dest='path', type=str,
                         default='../seq2seq/init_dataset/PNG/path')
     parser.add_argument('--maxlen', dest='maxlen', type=int, default=1000)
@@ -36,18 +36,19 @@ if __name__=='__main__':
     print('- MODEL:', args.model)
     print('- MODE:', args.mode)
     print('- MAXLEN:', MAXLEN)
-    print('- EMBEDDING_DIM:', EMBEDDING_DIM)
     print('- BATCH_SIZE:', BATCH_SIZE)
     print('- EPOCHS:', EPOCHS)
     print('- PATIENCE:', PATIENCE)
+    print('- EMBEDDING_DIM:', EMBEDDING_DIM)
     print('- DATASET_PATH:', DATASET_PATH)
 
     input_tensor, target_tensor = load_dataset(DATASET_PATH, pad_maxlen=MAXLEN)
 
+    # Seq2Seq Model
     if args.model == 'seq2seq':
         print('\n[*] Start simple seq2seq model ...')
 
-        train_ds, test_ds = split_tensor(input_tensor, target_tensor, batch_size=BATCH_SIZE, test_ratio=0)
+        train_ds, test_ds = split_tensor(input_tensor, target_tensor, batch_size=BATCH_SIZE, test_ratio=0.0)
         model = Seq2seq(ENC_VOCAB_SIZE, DEC_VOCAB_SIZE, embedding_dim=EMBEDDING_DIM,
                         units=256, sos=SOS, eos=EOS, maxlen=MAXLEN)
 
@@ -77,32 +78,76 @@ if __name__=='__main__':
         else:
             model.load_weights(fullname)
             test_seq2seq_model(model, test_ds, verbose=False, save='./output/PNG/seq2seq/')
+    # Attention Model
     elif args.model == 'attention':
         print('\n[*] Start seq2seq with attention model ...')
 
-        train_ds, test_ds = split_tensor(input_tensor, target_tensor,
-                                         batch_size=BATCH_SIZE,
-                                         test_ratio=0.2)
-
-        model = Attentions(ENC_VOCAB_SIZE, DEC_VOCAB_SIZE,
-                           embedding_dim=EMBEDDING_DIM, units=256,
+        train_ds, test_ds = split_tensor(input_tensor, target_tensor, batch_size=BATCH_SIZE, test_ratio=0.0)
+        model = Attentions(ENC_VOCAB_SIZE, DEC_VOCAB_SIZE, embedding_dim=EMBEDDING_DIM, units=256,
                            sos=SOS, eos=EOS, maxlen=MAXLEN)
 
-        model = train_seq2seq_model(model, train_ds, EPOCHS, early_stop_patience=PATIENCE)
-        test_seq2seq_model(model, test_ds, verbose=True, save=False)
+        dir_path = './model/attention'
+        filename = 'attention_model_latest'
+        fullname = os.path.join(dir_path, filename)
 
+        if args.mode == 'train':
+            model = train_seq2seq_model(model, train_ds, EPOCHS, early_stop_patience=PATIENCE)
+            if (os.path.isdir(dir_path) == False):
+                os.mkdir(dir_path)
+
+            cpfile = os.path.join(dir_path, 'checkpoint')
+            if (os.path.isfile(cpfile) == True):
+                filetime = time.strftime("_%Y%m%d-%H%M%S")
+                newcp = cpfile + filetime
+                fullname1 = fullname + '.index'
+                fullname2 = fullname + '.data-00000-of-00001'
+                newname1 = fullname + filetime + '.index'
+                newname2 = fullname + filetime + '.data-00000-of-00001'
+
+                os.rename(cpfile, newcp)
+                os.rename(fullname1, newname1)
+                os.rename(fullname2, newname2)
+
+            model.save_weights(fullname)
+        else:
+            model.load_weights(fullname)
+            test_seq2seq_model(model, test_ds, verbose=False, save='./output/PNG/attention/')
+    # Transformer Model
     elif args.model == 'transformer':
         print('\n[*] Start transformer model ...')
 
-        train_ds, test_ds = split_tensor(input_tensor, target_tensor,
-                                         batch_size=BATCH_SIZE,
-                                         test_ratio=0.2, algo='transformer')
+        train_ds, test_ds = split_tensor(input_tensor, target_tensor, batch_size=BATCH_SIZE,
+                                         algo='transformer', test_ratio=0.0)
 
         model = transformer(input_dim=MAXLEN, num_layers=4, dff=512,
                             d_model=EMBEDDING_DIM, num_heads=4, dropout=0.3, name="transformer")
 
-        model = train_transformer_model(model, train_ds, EPOCHS, MAXLEN, early_stop_patience=PATIENCE)
-        test_transformer_model(model, test_ds, MAXLEN, verbose=True, save=False)
+        dir_path = './model/transformer'
+        filename = 'transformer_model_latest'
+        fullname = os.path.join(dir_path, filename)
+
+        if args.mode == 'train':
+            model = train_transformer_model(model, train_ds, EPOCHS, MAXLEN, early_stop_patience=PATIENCE)
+            if (os.path.isdir(dir_path) == False):
+                os.mkdir(dir_path)
+
+            cpfile = os.path.join(dir_path, 'checkpoint')
+            if (os.path.isfile(cpfile) == True):
+                filetime = time.strftime("_%Y%m%d-%H%M%S")
+                newcp = cpfile + filetime
+                fullname1 = fullname + '.index'
+                fullname2 = fullname + '.data-00000-of-00001'
+                newname1 = fullname + filetime + '.index'
+                newname2 = fullname + filetime + '.data-00000-of-00001'
+
+                os.rename(cpfile, newcp)
+                os.rename(fullname1, newname1)
+                os.rename(fullname2, newname2)
+
+            model.save_weights(fullname)
+        else:
+            model.load_weights(fullname)
+            test_transformer_model(model, test_ds, MAXLEN, verbose=False, save='./output/PNG/transformer/')
     else:
         print('Please enter the right model name')
         print('python3 ./ddrfuzz.py --model [seq2seq|attention|transformer]')
